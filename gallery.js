dcf8bb686971b97987a53a6259ed9613b3a5935e
Harrison Patton <harry.belden@gmail.com>
2023-02-21 20:29:17 -0600

    clean up JS gallery code, condense code into updateMainImage function and added functionality for arrow key presses

--- gallery.js
+++ gallery.js
@@ -7,42 +7,49 @@
     rightArrow = document.querySelector('.material-icons.right-btn');
   let currentIndex = 0;
 
+  // function to update the main image and caption
+  function updateMainImage(index) {
+    imageMain.src = thumbnails[index].src.replace('150/100', '1000/400');
+    captionElement.textContent = thumbnails[index].dataset.caption;
+    imageMain.alt = thumbnails[index].dataset.caption;
+    imageMain.title = thumbnails[index].dataset.caption;
+  }
+
   for (let i = 0; i < thumbnails.length; i++) {
     thumbnails[i].addEventListener('click', function (evt) {
-      imageMain.src = evt.target.src.replace('150/100', '1000/400'); //Replaces the smaller image size with a larger size
       currentIndex = evt.target.id.substr(6, evt.target.id.length) - 1; //Updates current index by getting the substring of the ID attribute of the gallery__img, starting at the 6th character (which is the number) and ending at the end of the string. We then subtract 1 from this so that is can be zero based, otherwise the JS will display the image that is one number higher, since arrays start at 0.
-      let caption = evt.target.dataset.caption;
-      //Update the caption element
-      captionElement.textContent = evt.target.dataset.caption;
-      imageMain.alt = caption;
-      imageMain.title = caption;
+      updateMainImage(currentIndex);
     });
   }
+
   leftArrow.addEventListener('click', function (evt) {
     if (currentIndex > 0) {
       currentIndex--;
-      imageMain.src = thumbnails[currentIndex].src.replace(
-        '150/100',
-        '1000/400'
-      );
-      // Retrieve the caption for the current thumbnail and update the imageMain caption accordingly
-      const caption = thumbnails[currentIndex].dataset.caption;
-      imageMain.alt = caption;
-      imageMain.title = caption;
+      updateMainImage(currentIndex);
     }
   });
 
   rightArrow.addEventListener('click', function (evt) {
     if (currentIndex < thumbnails.length - 1) {
       currentIndex++;
-      imageMain.src = thumbnails[currentIndex].src.replace(
-        '150/100',
-        '1000/400'
-      );
-      // Retrieve the caption for the current thumbnail and update the imageMain caption accordingly
-      const caption = thumbnails[currentIndex].dataset.caption;
-      imageMain.alt = caption;
-      imageMain.title = caption;
+      updateMainImage(currentIndex);
     }
   });
+
+  // add keydown event listener to window
+  window.addEventListener('keydown', function (evt) {
+    if (evt.key === 'ArrowLeft' && currentIndex > 0) {
+      currentIndex--;
+      updateMainImage(currentIndex);
+    } else if (
+      evt.key === 'ArrowRight' &&
+      currentIndex < thumbnails.length - 1
+    ) {
+      currentIndex++;
+      updateMainImage(currentIndex);
+    }
+  });
+
+  // initialize the main image and caption
+  updateMainImage(currentIndex);
 })();
