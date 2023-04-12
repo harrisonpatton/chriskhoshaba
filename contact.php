3fb50ef0cfab3f5b59fdf0f23fdb4bab6ed427fb
Harrison Patton <harry.belden@gmail.com>
2023-04-12 08:20:00 -0500

    comment out contact.php code

--- contact.php
+++ contact.php
@@ -1,19 +1,19 @@
-<?php
+<!-- <?php
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//   $name = $_POST['name'];
+//   $email = $_POST['email'];
+//   $message = $_POST['message'];
   
-  $to = "harry.belden@gmail.com";
-  $subject = "New Contact Form Submission";
-  $body = "Name: $name\nEmail: $email\nMessage: $message";
+//   $to = "harry.belden@gmail.com";
+//   $subject = "New Contact Form Submission";
+//   $body = "Name: $name\nEmail: $email\nMessage: $message";
   
-  if (mail($to, $subject, $body)) {
-    echo "Your message has been sent successfully.";
-  } else {
-    echo "An error occurred while sending your message.";
-  }
-}
+//   if (mail($to, $subject, $body)) {
+//     echo "Your message has been sent successfully.";
+//   } else {
+//     echo "An error occurred while sending your message.";
+//   }
+// }
 
-?>
+?> -->
